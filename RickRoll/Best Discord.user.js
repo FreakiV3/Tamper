2b39fbd1f06267c74555090f504e24fd9069aa0a
FreakiV3 <135858469+FreakiV3@users.noreply.github.com>
2023-08-19 12:27:28 +0200

    Best Discord.user.js

--- RickRoll/Best Discord.user.js
+++ RickRoll/Best Discord.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Better discord animations
-// @author       You
+// @author       Freakidann
 // @match        https://discord.com/*
 // @grant        none
 // ==/UserScript==
